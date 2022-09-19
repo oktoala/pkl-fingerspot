e4bff6728b8136d4651099054324445a3c0902d6
oktoala <okta.yoga.syah@gmail.com>
2022-09-19 16:12:41 +0800

    Able to auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,43 +1,38 @@
-// import { PrismaClient } from "@prisma/client";
-import { RequestInternal, Awaitable, User } from "next-auth";
-import type { JWT } from "next-auth/jwt";
-import type { Session } from "next-auth";
-import NextAuth from "next-auth/next";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaClient } from "@prisma/client";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-export default NextAuth({
+const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     CredentialsProvider({
-      name: "Credentials",
+      type: "credentials",
       credentials: {},
-      authorize: async function (
-        credentials: Record<never, string> | undefined,
-        req: Pick<RequestInternal, "query" | "headers" | "body" | "method">
-      ): Promise<Omit<User, "id"> | { id?: string | undefined } | null> {
-        console.log(req.body?.nip);
-        throw new Error("Function not implemented.");
+      async authorize(credentials, req) {
+        const { password } = credentials as {
+          password: string;
+        };
+
+        const user = await prisma.pegawai.findUnique({
+          where: {
+            pegawai_pin: password,
+          },
+        });
+
+        if (user && password === user.pegawai_nip) {
+          return user;
+        }
+      throw new Error('Error Login');
       },
     }),
   ],
-  callbacks: {
-    jwt: async function (params): Promise<JWT> {
-      const { token, user } = params;
-      if (user) {
-        token.name = user.name;
-      }
-      return token;
-    },
-    session: async  (params): Promise<Session> => {
-      const {session, token} = params;
-      session.user = {
-        name: token.name
-      };
-      return session;
-    }
-  },
   pages: {
-    signIn: "login"
-  }
-});
+    signIn: "/login",
+  },
+};
+
+export default NextAuth(authOptions);
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,65 +1,20 @@
 // import {  PrismaClient } from "@prisma/client";
-import {
-  ClientSafeProvider,
-  getProviders,
-  getSession,
-  GetSessionParams,
-  LiteralUnion,
-  signIn,
-} from "next-auth/react";
+import { NextPage } from "next";
 import Head from "next/head";
-import { BuiltInProviderType } from "next-auth/providers";
+import { signIn } from "next-auth/react";
+import { FormEventHandler, useState } from "react";
+const Login: NextPage = (props): JSX.Element => {
+  const [user, setUser] = useState({ password: "" });
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
 
-// const prisma = new PrismaClient();
+    const res = await signIn("credentials", {
+      password: user.password,
+      redirect: false
+    },);
 
-export const getServerSideProps = async (context: GetSessionParams) => {
-  const session = await getSession(context);
-
-  if (session) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/",
-      },
-    };
-  }
-  return {
-    props: {
-      providers: await getProviders(),
-    },
+    console.log(res);
   };
-};
-
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   const pegawaii = await prisma.pegawai.findMany({
-//     select: {
-//       pegawai_nama: true,
-//     },
-//   });
-//   return {
-//     props: {
-//       pegawaii: JSON.parse(JSON.stringify(pegawaii)),
-//     },
-//   };
-// };
-// type Props = {
-//   pegawaii: pegawai[];
-// };
-
-type provider = {
-  providers: Record<
-    LiteralUnion<BuiltInProviderType, string>,
-    ClientSafeProvider
-  >;
-};
-
-function login(e: any): void {
-  signIn(e.target.credentialsID.value, {
-    nip: e.target.nip.value,
-  });
-}
-
-const Login: React.FC<provider> = ({ providers }) => {
   return (
     <>
       <Head>
@@ -88,85 +43,28 @@ const Login: React.FC<provider> = ({ providers }) => {
             Silahkan Login Dulu
           </div>
           <div className="mt-10">
-            <form method="POST" onSubmit={login}>
-              <input
-                type="hidden"
-                name="credentialsID"
-                value={providers.credentials.id}
-              />
+            <form method="POST" onSubmit={handleSubmit}>
+              <input type="hidden" name="credentialsID" />
               <div className="flex flex-col mb-5">
                 <label className="mb-1 text-xs tracking-wide text-gray-600">
                   Nomor Induk Pegawai
                 </label>
                 <div className="relative">
-                  <div
-                    className="
-                    inline-flex
-                    items-center
-                    justify-center
-                    absolute
-                    left-0
-                    top-0
-                    h-full
-                    w-10
-                    text-gray-400
-                  "
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z"
-                      />
-                    </svg>
+                  <div className="icon">
+                    <NIMIcon />
                   </div>
                   <input
-                    id="nip"
-                    type="text"
-                    name="nip"
-                    className="
-                    text-sm
-                    placeholder-gray-500
-                    pl-10
-                    pr-4
-                    rounded-2xl
-                    border border-gray-400
-                    w-full
-                    py-2
-                    focus:outline-none focus:border-blue-400
-                  "
+                    type="password"
+                    name="password"
+                    className="input"
                     placeholder="Masukkan NIP"
+                    value={user.password}
+                    onChange={(e) => setUser({ password: e.target.value })}
                   />
                 </div>
               </div>
               <div className="flex w-full">
-                <button
-                  type="submit"
-                  className="
-                  flex
-                  mt-2
-                  items-center
-                  justify-center
-                  focus:outline-none
-                  text-white text-sm
-                  sm:text-base
-                  bg-blue-500
-                  hover:bg-blue-600
-                  rounded-2xl
-                  py-2
-                  w-full
-                  transition
-                  duration-150
-                  ease-in
-                "
-                >
+                <button type="submit" className="btn-submit">
                   <span className="mr-2 uppercase">Sign In</span>
                   <span>
                     <svg
@@ -188,6 +86,25 @@ const Login: React.FC<provider> = ({ providers }) => {
         </div>
       </div>
     </>
+  );
+};
+
+const NIMIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z"
+      />
+    </svg>
   );
 };
 
